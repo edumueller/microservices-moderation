d802cdf0e246046fc14b75c8227b39a960542f1e
edumueller <eduardo.nicastro@icloud.com>
2021-08-16 20:31:18 -0300

    feat(events): add comment moderation logic

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ const app = express();
 app.use(bodyParser.json());
 const PORT = 4003;
 
-app.post("/events", (req, res) => {});
+app.post(
+  "/events",
+  async (
+    {
+      body: {
+        type,
+        data: { id, postId, status, content },
+      },
+    },
+    res
+  ) => {
+    if (type === "CommentCreated") {
+      status = data.content.includes("orange") ? "rejected" : "approved";
+      await axios.post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: { id, postId, status, content },
+      });
+    }
+
+    res.send({});
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
